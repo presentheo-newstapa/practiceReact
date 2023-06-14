672515b987bea3a4174d54836b4aa54d6ba33607
presentheo-newstapa <presentheo@newstapa.org>
2023-06-14 17:32:21 +0900

    asd

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg's
+// import viteLogo from '/vite.svg's
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)s
+
+  return (
+    <>
+      <div>
+        <h1 className='text-3xl font-bold underline'>Hello World!</h1>
+
+      </div>
+    </>
+  )
+}
+
+export default App
